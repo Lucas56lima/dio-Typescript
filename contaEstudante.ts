bdce0baed5792eff794fac537aa54b004147a608
Lucas56lima <108679991+Lucas56lima@users.noreply.github.com>
2023-06-28 00:07:41 -0300

    Create contaEstudante.ts

---
+++ contaEstudante.ts
@@ -0,0 +1,10 @@
+import { ContaDio } from "./ContaDio";
+
+export class contaEstudante extends ContaDio{
+
+    constructor(nome:string,contaNumero:number){
+
+        super(nome,contaNumero)
+    }    
+    
+}
